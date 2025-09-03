ebc79cf1b6119397a874b124f25effe0fad62cf6
putriwulannurcahayani <putriwulannurcahayani02@gmail.com>
2025-09-03 22:22:50 +0700

    update footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import PartnerSection from "@/components/PartnerSection";
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <HeroSection />
       <PartnerSection />
+      <Footer />
     </>
   );
 }
